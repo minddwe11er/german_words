f551252dadd87e5d0cae52d6851e5179842cca8e
minddwe11er <vitaly.stemirov@gmail.com>
2025-10-28 21:16:22 +0100

    Add recaptcha fix

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,5 +1,6 @@
 import { Montserrat } from "next/font/google";
 import "./globals.css";
+import Script from 'next/script';
 
 export const metadata = {
   title: "German word of the day",
@@ -11,11 +12,14 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <head>
-        <script src="https://www.google.com/recaptcha/enterprise.js?render=6LcIH-MrAAAAAJBwfUPeSxwGZOUZOS5Yavh1GF2X"></script>
+        <Script
+          src={`https://www.google.com/recaptcha/api.js?render=${process.env.NEXT_PUBLIC_RECAPTCHA_SITE_KEY}`}
+          strategy="afterInteractive"
+          async
+          defer
+        />
       </head>
-      <body>
-        {children}
-      </body>
+      <body>{children}</body>
     </html>
   );
 }
--- src/lib/recaptcha.js
+++ src/lib/recaptcha.js
@@ -1,10 +1,11 @@
-export const verifyRecaptcha = async (token) => {
-    const secret = process.env.RECAPTCHA_SECRET_KEY;
-    const response = await fetch('https://www.google.com/recaptcha/api/siteverify', {
-        method: 'POST',
-        headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
-        body: new URLSearchParams({ secret, response: token }),
-    });
-    const data = await response.json();
-    return data.success && data.score > 0.5;
+export const createAssessment = async (token) => {
+  const secret = process.env.RECAPTCHA_SECRET_KEY;
+  if (!secret) return { success: false, error: 'Secret key not found' };
+  const response = await fetch('https://www.google.com/recaptcha/api/siteverify', {
+    method: 'POST',
+    headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
+    body: new URLSearchParams({ secret, response: token }),
+  });
+  const data = await response.json();
+  return { success: data.success, score: data.score };
 };
